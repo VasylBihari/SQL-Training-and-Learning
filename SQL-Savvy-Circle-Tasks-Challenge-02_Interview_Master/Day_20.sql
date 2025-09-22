e33fb4111f5793f4d97064720ae09be2a5b16256
VasylBihari <vasylbihari@gmail.com>
2025-09-22 14:10:24 +0300

    Update Day_20.sql

--- SQL-Savvy-Circle-Tasks-Challenge-02_Interview_Master/Day_20.sql
+++ SQL-Savvy-Circle-Tasks-Challenge-02_Interview_Master/Day_20.sql
@@ -1,1 +1,58 @@
+/*Retrieve the total marketing spend in each country for Q1 2024 to help inform budget distribution across regions.
+Tables
+fact_marketing_spend(spend_id, country_id, campaign_date, amount_spent)
+fact_daily_subscriptions(subscription_id, country_id, signup_date, num_new_subscribers)
+dimension_country(country_id, country_name)*/
 
+SELECT
+    d.country_name,
+    SUM(f.amount_spent) AS total_sum
+FROM dimension_country d 
+LEFT JOIN fact_marketing_spend f ON d.country_id = f.country_id
+WHERE f.campaign_date BETWEEN '2024-01-01' AND '2024-03-31'
+GROUP BY d.country_name;
+
+/*List the number of new subscribers acquired in each country (with name) during January 2024, renaming the subscriber count column to 'new_subscribers' for clearer reporting purposes.
+Tables
+fact_marketing_spend(spend_id, country_id, campaign_date, amount_spent)
+fact_daily_subscriptions(subscription_id, country_id, signup_date, num_new_subscribers)
+dimension_country(country_id, country_name)*/
+
+SELECT
+  country_name,
+  SUM(num_new_subscribers) AS new_subscribers
+FROM dimension_country d
+LEFT JOIN fact_daily_subscriptions f ON d.country_id=f.country_id
+WHERE signup_date BETWEEN '2024-01-01' AND '2024-01-31'
+GROUP BY country_name
+
+/*Determine the average marketing spend per new subscriber for each country in Q1 2024 by rounding up to the nearest whole number to evaluate campaign efficiency.
+Tables
+fact_marketing_spend(spend_id, country_id, campaign_date, amount_spent)
+fact_daily_subscriptions(subscription_id, country_id, signup_date, num_new_subscribers)
+dimension_country(country_id, country_name)*/
+
+WITH spent_table AS (SELECT 
+  d.country_name AS country,
+  SUM(amount_spent) AS total_spent
+FROM dimension_country d 
+LEFT JOIN fact_daily_subscriptions f ON f.country_id=d.country_id
+LEFT JOIN fact_marketing_spend s ON d.country_id=s.country_id
+WHERE s.campaign_date BETWEEN '2024-01-01' AND '2024-03-31'
+GROUP BY d.country_name
+  ),
+num_subscr_table AS (
+  SELECT 
+  d.country_name AS country,
+  SUM(num_new_subscribers) AS total_num_subscr
+FROM dimension_country d 
+LEFT JOIN fact_daily_subscriptions f ON f.country_id=d.country_id
+LEFT JOIN fact_marketing_spend s ON d.country_id=s.country_id
+WHERE f.signup_date BETWEEN '2024-01-01' AND '2024-03-31'
+GROUP BY d.country_name
+)
+SELECT
+  s.country,
+  CEIL(total_spent/total_num_subscr) AS average_by_country
+FROM spent_table s 
+LEFT JOIN num_subscr_table n ON s.country=n.country
