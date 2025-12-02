bb91a5aebc84735d394f19ac7d91c77da8c59e40
VasylBihari <vasylbihari@gmail.com>
2025-12-02 11:09:26 +0200

    Update Day 1.sql

--- SQL Advent Calendar_Interview Master/Day 1.sql
+++ SQL Advent Calendar_Interview Master/Day 1.sql
@@ -1,1 +1,10 @@
+/* Every year, the city of Whoville conducts a Reindeer Run to find the best reindeers for Santa's Sleigh. Can you write a query to return the name and rank of the top 7 reindeers in this race?
+Tables
+reindeer_run_results(number, name, rank, color) */
 
+SELECT
+  name,
+  rank
+FROM reindeer_run_results
+WHERE rank <= 7
+ORDER BY rank 
