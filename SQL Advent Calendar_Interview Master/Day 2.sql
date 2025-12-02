c99c9e24579030beffa12a21f670470fe31ebab5
VasylBihari <vasylbihari@gmail.com>
2025-12-02 19:13:44 +0200

    Update Day 2.sql

--- SQL Advent Calendar_Interview Master/Day 2.sql
+++ SQL Advent Calendar_Interview Master/Day 2.sql
@@ -1,1 +1,12 @@
+/*Santa wants to analyze which toys that were produced in his workshop have already been delivered to children. You are given two tables on toy production and toy delivery — can you return the toy ID and toy name of the toys that have been delivered?
+Tables
+toy_production(toy_id, toy_name, production_date)
+toy_delivery(toy_id, child_name, delivery_date)*/
 
+SELECT
+  d.toy_id,
+  p.toy_name,
+  d.delivery_date
+FROM toy_delivery d
+INNER JOIN toy_production p ON p.toy_id=d.toy_id
+  WHERE d.delivery_date < CURRENT_DATE
