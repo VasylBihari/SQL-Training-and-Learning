3d269c47a47ed8f154840ba27b7338f09a1c0e99
VasylBihari <vasylbihari@gmail.com>
2025-12-09 11:28:12 +0200

    Update Day 7.sql

--- SQL Advent Calendar_Interview Master/Day 7.sql
+++ SQL Advent Calendar_Interview Master/Day 7.sql
@@ -1,1 +1,8 @@
+/*Frosty wants to know how many unique snowflake types were recorded on the December 24th, 2025 storm. Can you help him?
+Tables
+snowfall_log(flake_id, flake_type, fall_time)*/
 
+SELECT
+  COUNT(DISTINCT(flake_type)) AS count_flake_type
+FROM snowfall_log
+WHERE fall_time >='2025-12-24' AND fall_time <'2025-12-25'
