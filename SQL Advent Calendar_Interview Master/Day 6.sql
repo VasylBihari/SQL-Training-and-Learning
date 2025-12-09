0579cdc5a7f5f962b18297ae03d4392aff851c0e
VasylBihari <vasylbihari@gmail.com>
2025-12-09 11:03:12 +0200

    Update Day 6.sql

--- SQL Advent Calendar_Interview Master/Day 6.sql
+++ SQL Advent Calendar_Interview Master/Day 6.sql
@@ -1,1 +1,11 @@
+/*Buddy is planning a winter getaway and wants to rank ski resorts by annual snowfall. 
+Can you help him bucket these ski resorts into quartiles?
+Tables
+resort_monthly_snowfall(resort_id, resort_name, snow_month, snowfall_inches)*/
 
+SELECT
+  resort_name,
+  SUM(snowfall_inches) AS total_year_snowfall,
+  NTILE(4) OVER (ORDER BY SUM(snowfall_inches)) AS quartile
+FROM resort_monthly_snowfall
+GROUP BY resort_name
