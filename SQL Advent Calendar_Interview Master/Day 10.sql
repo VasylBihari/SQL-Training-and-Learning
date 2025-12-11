ffa23018f144afd2313a10792e53757e56b7907f
VasylBihari <vasylbihari@gmail.com>
2025-12-11 11:59:03 +0200

    Update Day 10.sql

--- SQL Advent Calendar_Interview Master/Day 10.sql
+++ SQL Advent Calendar_Interview Master/Day 10.sql
@@ -1,1 +1,11 @@
+/*In the holiday cookie factory, workers are measuring how efficient each oven is. 
+Can you find the average baking time per oven rounded to one decimal place?
+Tables
+cookie_batches(batch_id, oven_id, baking_time_minutes)*/
 
+SELECT
+  oven_id,
+  ROUND(AVG(baking_time_minutes),1) AS avg_time_per_oven
+FROM cookie_batches
+GROUP BY oven_id
+ORDER BY avg_time_per_oven DESC
