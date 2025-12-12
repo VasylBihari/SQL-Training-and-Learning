0f89661fb3c74f3ec8b59dd2bacc108ea3d451e3
VasylBihari <vasylbihari@gmail.com>
2025-12-12 12:56:46 +0200

    Update Day 11.sql

--- SQL Advent Calendar_Interview Master/Day 11.sql
+++ SQL Advent Calendar_Interview Master/Day 11.sql
@@ -1,1 +1,10 @@
+/*At the winter market, Cindy Lou is browsing the clothing inventory and wants to find all items with "sweater" in their name. 
+But the challenge is the color and item columns have inconsistent capitalization. Can you write a query to return only the sweater names and their cleaned-up colors.
+Tables
+winter_clothing(item_id, item_name, color)*/
 
+SELECT
+    item_name,
+    INITCAP(color) AS cleaned_color
+FROM winter_clothing
+WHERE item_name ILIKE '%sweater%'
