02e852fd11604b8201e006b794a74d4583cda8e2
VasylBihari <vasylbihari@gmail.com>
2025-12-15 13:46:40 +0200

    Update Day 14.sql

--- SQL Advent Calendar_Interview Master/Day 14.sql
+++ SQL Advent Calendar_Interview Master/Day 14.sql
@@ -1,1 +1,11 @@
+/*The Productivity Club is tracking members' challenge start dates and wants to calculate each member's focus_end_date, exactly 14 days after their start date. 
+Can you write a query to return the existing table with the focus_end_date column?
+Tables
+focus_challenges(member_id, member_name, start_date)*/
 
+SELECT
+  member_id,
+  member_name,
+  start_date,
+  start_date + 14 AS focus_end_date
+FROM focus_challenges
