4ab8e9ab4cb94710a3e8c2149e74ac90793b1083
VasylBihari <vasylbihari@gmail.com>
2025-12-16 08:40:00 +0200

    Update Day 15.sql

--- SQL Advent Calendar_Interview Master/Day 15.sql
+++ SQL Advent Calendar_Interview Master/Day 15.sql
@@ -1,1 +1,11 @@
+/*The Grinch is tracking his daily mischief scores to see how his behavior changes over time. 
+Can you find how many points his score increased or decreased each day compared to the previous day?
+Tables
+grinch_mischief_log(log_date, mischief_score)*/
 
+SELECT
+    log_date,
+    mischief_score,
+    LAG(mischief_score) OVER (ORDER BY log_date) AS prev_revenue,
+    mischief_score - LAG(mischief_score) OVER (ORDER BY log_date) AS diff
+FROM grinch_mischief_log
